6f62b1e72329b6c973e785156c513cc800852f0f
elenaspasumir <167351289+elenaspasumir@users.noreply.github.com>
2024-04-24 14:14:09 +0000

    Прыжок Бельчи

--- script.js
+++ script.js
@@ -2,9 +2,20 @@ let snail = document.querySelector("#snail");
 let snailCoordinate = 730;
 let renderSnail = function() {
   snail.style.left = snailCoordinate + "px";
-  snailCoordinate -= 1;
+  snailCoordinate -= 2;
   if(snailCoordinate <= -60) {
     snailCoordinate = 730;
   }
 }
-setInterval(renderSnail, 1000/24);
+setInterval(renderSnail, 1000/60);
+
+let belcha = document.querySelector("#belcha");
+let landBelcha = function() {
+  belcha.style.bottom = "0px";
+}
+window.onkeydown = function (evt) {
+  if (evt.code == "Space") {
+    belcha.style.bottom = "80px";
+    setTimeout(landBelcha, 1500); 
+  }
+};
